4af1cf3a93dcb62b8733ce26b718ef758631077d
Matus Kobolka <kobolka.matus@gmail.com>
2020-05-06 15:54:21 +0200

    kiosk mode for chromium startup

--- kiosk.sh
+++ kiosk.sh
@@ -4,4 +4,9 @@ xset s noblank
 xset s off
 xset -dpms
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required file:///home/pi/pi_kiosk/player/player.html
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html
