0f5e245ba6b338a2bf90b54ffd2edf6aceb8318a
Matus Kobolka <kobolka.matus@gmail.com>
2020-05-07 16:15:55 +0200

    kiosk starting

--- kiosk.sh
+++ kiosk.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-xset s noblank
-xset s off
-xset -dpms
+xset -dpms       # Turn off dpms blanking until next boot
+xset s activate  # Force screen blank
+xset s 1
 
 unclutter -idle 0.5 -root &
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html &
+
+sleep 10         # Allow time for booting and starting browser. Adjust as necessary
+xset s reset     # Force screen on
+xset s 0         # Disable blanking until next boot
