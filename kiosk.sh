db551aefe6504ef184c646d3abb72f65084fef10
Matus Kobolka <kobolka.matus@gmail.com>
2020-05-07 16:54:26 +0200

    kiosk start

--- kiosk.sh
+++ kiosk.sh
@@ -3,7 +3,6 @@
 export DISPLAY=:0
 export XAUTHORITY=~/.Xauthority
 
-xset -dpms       # Turn off dpms blanking until next boot
 xset s activate  # Force screen blank
 xset s 1
 
@@ -14,7 +13,9 @@ sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium
 
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html &
 
-sleep 10         # Allow time for booting and starting browser. Adjust as necessary
-xset s reset && xset dpms force on # Force screen on
-xset s 0         # Disable blanking until next boot
+sleep 10
+xset s reset && xset dpms force on
+xset s noblank
+xset s off
+xset -dpms
 
