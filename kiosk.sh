47c5e51986b7dabdb5c77568ccf26cf93716c861
Matus Kobolka <kobolka.matus@gmail.com>
2020-05-07 18:18:40 +0200

    screen blanking

--- kiosk.sh
+++ kiosk.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-xset s noblank
-xset s off
-xset -dpms
+xset -display :0 dpms force off
 
 unclutter -idle 0.5 -root &
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html &
+
+sleep 10
+
+xset -display :0 dpms force on
+
+xset s noblank
+xset s off
+xset -dpms
