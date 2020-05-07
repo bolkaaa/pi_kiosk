e20665e019f9f8929d0387e8086615e9721944c3
Matus Kobolka <kobolka.matus@gmail.com>
2020-05-07 16:59:37 +0200

    kiosk starting up

--- kiosk.sh
+++ kiosk.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 export DISPLAY=:0
-export XAUTHORITY=~/.Xauthority
 
 xset s activate  # Force screen blank
 xset s 1
@@ -14,7 +12,7 @@ sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html &
 
 sleep 10
-xset s reset && xset dpms force on
+xset s reset
 xset s noblank
 xset s off
 xset -dpms
