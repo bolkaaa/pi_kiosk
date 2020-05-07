4accf82f21d746b5fd0e6988ab04e6d842f8196f
Matus Kobolka <kobolka.matus@gmail.com>
2020-05-07 16:26:53 +0200

    kiosk starting

--- kiosk.sh
+++ kiosk.sh
@@ -9,7 +9,7 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html &
 
 sleep 10         # Allow time for booting and starting browser. Adjust as necessary
 xset s reset     # Force screen on
