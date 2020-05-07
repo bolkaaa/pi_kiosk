01994e98778d29e8b4d845079bddba9d2ba2e554
Matus Kobolka <kobolka.matus@gmail.com>
2020-05-07 17:03:35 +0200

    kiosk starting up

--- kiosk.sh
+++ kiosk.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-export DISPLAY=:0
 
-xset s activate  # Force screen blank
-xset s 1
+git fetch --all
+git reset --hard origin/master
+git pull origin master
+
+xset s noblank
+xset s off
+xset -dpms
 
 unclutter -idle 0.5 -root &
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html &
-
-sleep 10
-xset s reset
-xset s noblank
-xset s off
-xset -dpms
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html
 
