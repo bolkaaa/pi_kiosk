2c6ad1e7a8d66d19e4546327f8e382ab7f395042
Matus Kobolka <kobolka.matus@gmail.com>
2020-05-07 16:46:16 +0200

    kiosk starting

--- kiosk.sh
+++ kiosk.sh
@@ -12,5 +12,6 @@ sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html &
 
 sleep 10         # Allow time for booting and starting browser. Adjust as necessary
-xset s reset     # Force screen on
+xset s reset && xset dpms force on # Force screen on
 xset s 0         # Disable blanking until next boot
+
