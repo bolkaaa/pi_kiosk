6465a6761ec1c2458c4ea4adc1f35d21c837d201
Matus Kobolka <kobolka.matus@gmail.com>
2020-06-01 20:13:39 +0200

    kiosk mode

--- kiosk.sh
+++ kiosk.sh
@@ -9,4 +9,4 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required file:///home/pi/pi_kiosk/player/player.html
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --kiosk file:///home/pi/pi_kiosk/player/player.html
