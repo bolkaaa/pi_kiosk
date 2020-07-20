6d298c5806d2394dab43f69fb0529a891ef3a10f
Matus Kobolka <kobolka.matus@gmail.com>
2020-07-20 18:43:12 +0200

    ignore update dialog

--- kiosk.sh
+++ kiosk.sh
@@ -9,4 +9,4 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --kiosk --enable-kiosk-mode --enabled --touch-events --touch-events-ui --disable-ipv6 --disable-java --disable-restore-session-state --disable-sync --disable-translate --disk-cache-size=1 --media-cache-size=1 --max-disk-cache=5000000 --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --incognito file:///home/pi/pi_kiosk/player/player.html
+/usr/bin/chromium-browser --check-for-update-interval=31536000 --kiosk --enable-kiosk-mode --enabled --touch-events --touch-events-ui --disable-ipv6 --disable-java --disable-restore-session-state --disable-sync --disable-translate --disk-cache-size=1 --media-cache-size=1 --max-disk-cache=5000000 --noerrdialogs --disable-infobars --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --incognito file:///home/pi/pi_kiosk/player/player.html
